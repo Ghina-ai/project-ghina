98059e6badc7fac10a82e4c27a6fe4a6d9535ba9
Ghina <sistyaavinza@gmail.com>
2025-12-06 16:37:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('.contact-form');
+const nama = document.querySelector('input[name="name"]');
+const email = document.querySelector('input[email="email"]');
+const pesan = document.querySelector('textarea[message="message"]');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for your message!');
+    nama.value = '';
+    email.value = '';
+    pesan.value = '';
+});
+
